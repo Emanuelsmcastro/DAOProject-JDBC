c8d2b0aa8ad2ff26f725bd5a39f7a18a41448d79
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-11 18:09:20 -0300

    test seller

--- src/application/App.java
+++ src/application/App.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department dp = new Department(1, "Lib");
         System.out.println(dp);
+        Seller seller = new Seller(21, "Emanuel", "emanuel@gmail.com", new Date(), 3000.0, dp);
+        System.out.println(seller);
     }
 }
