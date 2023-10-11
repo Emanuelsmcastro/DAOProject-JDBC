2b03a10bbd379557eb74a753ce08b0e6ee03aae8
Emanuel Castro <emanuelsmcastro3@gmail.com>
2023-10-11 18:33:33 -0300

    Update main to test DaoFactory

--- src/application/App.java
+++ src/application/App.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoBase;
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,5 +13,6 @@ public class App {
         System.out.println(dp);
         Seller seller = new Seller(21, "Emanuel", "emanuel@gmail.com", new Date(), 3000.0, dp);
         System.out.println(seller);
+        DaoBase<Seller> sellerDao = DaoFactory.createSellerDao();
     }
 }
